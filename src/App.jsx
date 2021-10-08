66a70c1052ad0aa0f38eb2d8b99b081061dce505
Sarah Diba Anissa <sarahdiba29@gmail.com>
2021-10-08 12:56:07 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [item, setItem] = useState(null);
+  return <div className="App"> holla</div>;
+}
+
+export default App;
