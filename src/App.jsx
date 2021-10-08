66c3707f82b35f9da6d049baafb4544d1ab64226
Sarah Diba Anissa <sarahdiba29@gmail.com>
2021-10-08 16:03:56 +0200

    fetch shoppling backlog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+// see manual here : https://designcode.io/react-hooks-handbook-fetch-data-from-an-api
 function App() {
-  let [item, setItem] = useState(null);
-  return <div className="App"> holla</div>;
+  const [advice, setAdvice] = useState([]); // standartwert is ein leeres Array []
+  useEffect(() => {
+    const url = "https://fetch-me.vercel.app/shopping-list.json";
+    const backlog = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log("PRINT HERE!!!", json);
+
+        setAdvice(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    backlog();
+  }, []);
+
+  return (
+    <>
+      <div className="App">
+        <p>
+          {advice.map((advice) => (
+            <span key={advice.id}> {advice.name} </span>
+          ))}
+          ;
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
