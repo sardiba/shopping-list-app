c98cf6d49d52151e84241cdd23011706301831a2
Sarah Diba Anissa <sarahdiba29@gmail.com>
2021-10-10 21:31:36 +0200

    add handleclick function

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { ItemList } from "./components/ItemList";
 
-// see manual here : https://designcode.io/react-hooks-handbook-fetch-data-from-an-api
-function App() {
-  const [advice, setAdvice] = useState([]); // standartwert is ein leeres Array []
-  useEffect(() => {
-    const url = "https://fetch-me.vercel.app/shopping-list.json";
-    const backlog = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log("PRINT HERE!!!", json);
-
-        setAdvice(json);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    backlog();
-  }, []);
-
-  return (
-    <>
-      <div className="App">
-        <p>
-          {advice.map((advice) => (
-            <span key={advice.id}> {advice.name} </span>
-          ))}
-          ;
-        </p>
-      </div>
-    </>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <ItemList />;
+};
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+
+// see manual here : https://designcode.io/react-hooks-handbook-fetch-data-from-an-api
+export const ItemList = () => {
+  const [backlog, setBacklog] = useState([]); // standartwert is ein leeres Array []
+  const [toBuy, setToBuy] = useState([]);
+  useEffect(() => {
+    const url = "https://fetch-me.vercel.app/shopping-list.json";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log("PRINT HERE!!!", json);
+
+        setBacklog(json); //Array "Backlog" mit json ersetzen
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Array >> dependancy. wenn Array leer ist, wird die Funkiton useEffect einmal abgerufen
+
+  const handleClickBacklog = (event, item) => {
+    event.preventDefault();
+    console.log(event, item);
+    const newBacklogArray = backlog.filter(
+      (backlogItem) => backlogItem.id != item.id
+    ); //newArray besteht aus Elemente, die nicht geklickt wird >> != id
+    console.log(newBacklogArray);
+    setBacklog(newBacklogArray); // erstezen Backlog Array mit newBacklogArray
+    setToBuy([...toBuy, item]);
+  };
+  const handleClickToBuy = (event, item) => {
+    event.preventDefault();
+    console.log(event, item);
+    const newToBuyArray = toBuy.filter((toBuyItem) => toBuyItem.id != item.id);
+    console.log(newToBuyArray);
+    setToBuy(newToBuyArray);
+    setBacklog([...backlog, item]);
+  };
+  return (
+    <>
+      <div className="App">
+        <h1>Sarah's Shopping List </h1>
+        <h2>Backlog</h2>
+        <div>
+          {backlog.map((item) => (
+            <span
+              className="backlog"
+              onClick={(event) => handleClickBacklog(event, item)}
+              key={item.id}
+            >
+              {" "}
+              {item.name}{" "}
+            </span>
+          ))}
+        </div>
+        <h2>To Buy</h2>
+        <div>
+          {toBuy.map((item) => (
+            <span
+              className="toBuy"
+              onClick={(event) => handleClickToBuy(event, item)}
+              key={item.id}
+            >
+              {" "}
+              {item.name}{" "}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
